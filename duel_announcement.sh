0b312b9d47b9c84dff964dad2b196d6a1e85d627
alexe kun <acmedia.france@gmail.com>
2026-05-08 20:27:05 +0800

    first lab

---
+++ duel_announcement.sh
@@ -0,0 +1,11 @@
+first_contender="Merlin, the Mechanomancer"
+first_artifact="Codex of Infinite Loops"
+
+second_contender="Morgana, the Cyber Enchantress"
+second_artifact="Pendant of Parsed Prophecies"
+
+
+# ** should be replaced by the variables 
+
+echo " Prepare to witness a thrilling duel between $first_contender wielding his $first_artifact, and $second_contender with her $second_artifact. Let the arcane energies collide!
+"
---
+++ welcome_message.sh
@@ -0,0 +1,1 @@
+echo "Welcome to the Academy of Arcane Arts and Technomancy - Home of the annual Techno-Sorcery Tournament!"
